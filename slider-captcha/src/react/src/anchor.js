47fb6b72576bdd1d6500e984a180064504965f18
Wattana Tiemklang <osprior_wattana.tiemklang@krungthai.com>
2024-03-08 14:22:19 +0700

    new slider captcha

---
+++ slider-captcha/src/react/src/anchor.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from './card';
+
+const Anchor = ({
+  text,
+  fetchCaptcha,
+  submitResponse,
+  verified,
+}) => {
+  return (
+    <div>
+      {!verified && (
+        <div>
+          <Card
+            fetchCaptcha={fetchCaptcha}
+            submitResponse={submitResponse}
+            text={text}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+Anchor.propTypes = {
+  fetchCaptcha: PropTypes.func.isRequired,
+  submitResponse: PropTypes.func.isRequired,
+  text: PropTypes.shape({
+    anchor: PropTypes.string,
+    challenge: PropTypes.string,
+  }).isRequired,
+  verified: PropTypes.bool.isRequired,
+};
+
+export default Anchor;
---
+++ slider-captcha/src/react/src/theme.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import lightTheme from './style/light';
+
+const Theme = () => (
+  <style suppressHydrationWarning>{lightTheme}</style>
+);
+
+export default Theme;
